58e63f8b6d30190375a05d0da7c65b48ff1bfed9
Andjela Trajkovic <trajkovicka.9909@gmail.com>
2021-01-31 20:58:12 +0100

    Dodavanje gardova angular

--- dokumenti/sluzbenik-front/src/app/app.module.ts
+++ dokumenti/sluzbenik-front/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { NavbarGradjaninComponent } from './core/navbar-gradjanin/navbar-gradjanin.component';
+import { NavbarSluzbenikComponent } from './core/navbar-sluzbenik/navbar-sluzbenik.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Interceptor } from './interceptors/intercept.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    NavbarGradjaninComponent,
+    NavbarSluzbenikComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true}],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
