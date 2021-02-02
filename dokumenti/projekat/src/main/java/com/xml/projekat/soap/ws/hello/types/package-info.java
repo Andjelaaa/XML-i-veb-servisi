aef2fd614f9398a4a466948de5137a79b6ceb380
Andjela Trajkovic <trajkovicka.9909@gmail.com>
2021-02-03 00:26:47 +0100

    Soap konfiguracija

---
+++ dokumenti/projekat/src/main/java/com/xml/projekat/soap/ws/hello/types/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://xml.com/ws/hello/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.xml.projekat.soap.ws.hello.types;
